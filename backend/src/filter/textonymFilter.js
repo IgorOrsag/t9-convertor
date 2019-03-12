038b790d3bd0fd46c8952a76e20d710ff785f1bb
igor.orsag <igor.orsag@gmail.com>
2019-03-12 20:19:04 +0100

    O log n filter

--- backend/src/filter/textonymFilter.js
+++ backend/src/filter/textonymFilter.js
@@ -1,7 +1,16 @@
 const { corpus } = require('./corpus');
+const { search } = require('./binarySearch');
+
+const linearFilter = words => {
+  return words.filter(word => corpus.indexOf(word) !== -1);
+};
+
+const logFilter = words => {
+  return words.filter(word => search(word));
+};
 
 const filterTextonyms = words => {
-  return words.filter(word => corpus.indexOf(word) !== -1);
+  return logFilter(words);
 };
 
 module.exports = { filterTextonyms };
--- backend/src/middlewares/converter.js
+++ backend/src/middlewares/converter.js
@@ -2,9 +2,12 @@ const { getWords } = require('../converters/wordConverter');
 const { filterTextonyms } = require('../filter/textonymFilter');
 
 module.exports.converter = (req, res) => {
+  console.time('Conversion time');
   const converted = getWords(req.query.query);
   const textonyms = filterTextonyms(converted);
+  console.timeEnd('Conversion time');
   console.log('Converted words: ', converted);
   console.log('Filtered textonyms: ', textonyms);
+
   res.json({ converted, textonyms });
 };
